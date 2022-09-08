6243347338c817b33ba13ec769fece765efebf82
annabelleyu <annabelleyu@localhost>
2022-09-08 12:45:43 -0500

    Studio 1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a year: ");
+		int n1 = in.nextInt();
+		boolean LeapYear = (n1%4==0)&&(n1%100!=0)||(n1%400==0);
+		System.out.print(n1 + " is a leap year: "+ LeapYear);
+	}
+
+}
